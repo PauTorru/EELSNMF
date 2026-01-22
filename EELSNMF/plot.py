fe34b5fc4d50674e7ad6ce568f6a4193f9711692
PauTorru <pautorruellabesa@gmail.com>
2026-01-22 15:35:30 +0100

    Documentation of plot.py

--- EELSNMF/plot.py
+++ EELSNMF/plot.py
@@ -2,11 +2,12 @@ from .imports import *
 from .utils import *
 
 
-######################### many needs to modify everything
-
-
 class Plots:
+    """ Mixin class for all plot functionalities of EELSNMF object."""
 	def plot_factors(self):
+		"""
+		Plot EELSNMF factors spectral (mathematically the columns of G@W)
+		"""
 		plt.figure("Factors")
 		plt.clf()
 		for i in range(self.n_components):
@@ -16,6 +17,9 @@ class Plots:
 
 
 	def plot_loadings(self):
+		"""
+		Plot the aboundance maps of each component (mathematicall the rows of H)
+		"""
 		if not hasattr(self,"loadings"):
 			self.calculate_loadings()
 		if not hasattr(self,"plot_structure"):
@@ -33,6 +37,16 @@ class Plots:
 
 
 	def plot_edges(self,normalize=False):
+		"""
+		Plot the ELNES fit of each element and each component
+
+		Parameters
+		----------
+		normalize : bool
+			This flag rescales every ELNES from 0 to 1.
+		     (Default value = False)
+
+		"""
 		plt.figure("Edges")
 		plt.clf()
 		r,c=find_2factors(len(self.edges))
@@ -51,6 +65,18 @@ class Plots:
 
 
 	def plot_chemical_maps(self,quantified=True,method=None):
+		"""
+		Plot the maps associated to the elemental concentration of each element
+		Parameters
+		----------
+		quantified : bool
+			If true, normalize each pixel by the sum of all chemical signals on that pixel (output is % of a chemical elment at each pixel)
+		     (Default value = True)
+		method : function or str
+			Perform the quantification with this method, defaults are on EELSNMF.Analysis.default_q_methods
+		     (Default value = None)
+
+		"""
 		
 		qmaps=self.get_chemical_maps(quantified,method)
 		plt.figure("Quantified Chemical Maps")
@@ -64,6 +90,7 @@ class Plots:
 		plt.tight_layout()
 
 	def plot_average_model(self):
+		"""Plot mean spectral signal and mean model """
 		plt.figure("Model")
 		plt.clf()
 		plt.plot(self.energy_axis,self.X.mean(1),label="Data")
@@ -72,6 +99,8 @@ class Plots:
 		plt.tight_layout()
 
 	def plot_energy_ranges(self):
+		"""
+		Plot the energy ranges for each ELNES defined in self.fine_structure_ranges"""
 		plt.figure("Edges energy ranges")
 		plt.clf()
 		plt.plot(self.energy_axis,norm(self.X.mean(1)))
