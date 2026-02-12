350a3afd662d98b73572db382620985dfa4f2857
PauTorru <pautorruellabesa@gmail.com>
2026-02-12 13:36:22 +0100

    bugs previous commit

--- EELSNMF/modelG.py
+++ EELSNMF/modelG.py
@@ -68,7 +68,7 @@ class BaseModel:
 
 	def _undo_rescale(self):
 
-		self.G[:,:] *= self._scales[None,;]
+		self.G[:,:] *= self._scales[None,:]
 		self.W[:,:] /= self._scales[:,None]
 		"""Undo scaling after decomposition"""
 
