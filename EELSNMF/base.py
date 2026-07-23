8b498bbb2c66569e3cabccaee847bf5f63c6ed98
Ching Hei Mok <ching.mok@epfl.ch>
2026-07-23 11:03:56 +0200

    bug fix

--- EELSNMF/base.py
+++ EELSNMF/base.py
@@ -163,7 +163,9 @@ class EELSNMF(ModelG, Decomposition, Plots, Analysis):
 
         for m in self._m:
             if hasattr(self, m):
-                setattr(self, m, getattr(self, m).astype(dtype))
+                val = getattr(self, m)
+                if val is not None and hasattr(val, "astype"):
+                    setattr(self, m, val.astype(dtype))
         return
 
     def save(self, fname, path=None, save_hspy_objects=False, overwrite=False):
